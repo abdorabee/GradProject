99846aaa5011c3f951c857a1def630bc69ccf0f7
abdorabee <abdorabee1134@gmail.com>
2023-03-12 16:38:41 +0200

    nothing

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -2,16 +2,18 @@ import bcryptjs from 'bcryptjs';
 import { NextApiRequest,NextApiResponse } from 'next';
 import prismadb from '@/lib/prismadb';
 
-export default async function (req: NextApiRequest ,res:NextApiResponse){
+export default async function (req:NextApiRequest ,res:NextApiResponse){
     if(req.method !== 'POST'){
         return res.status(405).end()
     }
 
     try {
         const {email,name , password}=req.body;
+
+
         const existingUser = await prismadb.user.findUnique({
             where:{
-                email:email,
+                email,
             }
         });
         
