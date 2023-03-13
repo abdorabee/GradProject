d81d8e406443913b4129f1ad5f3cd2a897c9a11d
abdorabee <abdorabee1134@gmail.com>
2023-03-13 13:22:40 +0200

    just some changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,35 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+import { redirect } from "next/dist/server/api-utils";
+import "tailwindcss/tailwind.css";
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  // if (!session) {
+  //   return {
+  //     redirect: {
+  //       destination: "/auth",
+  //       permanent: false
+  //     }
+  //   };
+  // }
+
+  return {
+    props:{}
+  }
+}
 
 export default function Home() {
+  const {data:user}= useCurrentUser();
   return (
     <>
       <h1>BETA Movie</h1>
+      <p className="text-white">logged as : {user?.name}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Logout
+      </button>
     </>
-  )
+  );
 }
