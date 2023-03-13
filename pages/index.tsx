d2722e8c47acbfc871f66e66263070406a0ffb3c
abdorabee <abdorabee1134@gmail.com>
2023-03-13 18:05:41 +0200

    added the navbar and account menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 import { redirect } from "next/dist/server/api-utils";
 import "tailwindcss/tailwind.css";
 import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -24,11 +25,7 @@ export default function Home() {
   const { data: user } = useCurrentUser();
   return (
     <>
-      <h1>BETA Movie</h1>
-      <p className="text-white">logged as : {user?.name}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
-        Logout
-      </button>
+      <Navbar />
     </>
   );
 }
